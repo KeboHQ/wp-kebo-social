c4fcf23cfbd02318834b220d369f3f847e009812
peterbooker <peter@kebopowered.com>
2013-12-22 14:03:07 +0000

    default plugin basics

--- kebo_social.php
+++ kebo_social.php
@@ -29,44 +29,113 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+// Block direct access
+if ( ! defined( 'ABSPATH' ) ) {
+    die( 'Sorry, no direct access.' );
+}
+
 // Useful global constants
 define( 'KBSO_VERSION', '0.1.0' );
-define( 'KBSO_URL',     plugin_dir_url( __FILE__ ) );
-define( 'KBSO_PATH',    dirname( __FILE__ ) . '/' );
+define( 'KBSO_URL', plugin_dir_url(__FILE__) );
+define( 'KBSO_PATH', plugin_dir_path(__FILE__) );
+define( 'KBSO_API_URL', 'http://auth.kebopowered.com/request/request.php' );
 
-/**
- * Default initialization for the plugin:
- * - Registers the default textdomain.
+/*
+ * Load textdomain early, as we need it for the PHP version check.
  */
-function kbso_init() {
-	$locale = apply_filters( 'plugin_locale', get_locale(), 'kbso' );
-	load_textdomain( 'kbso', WP_LANG_DIR . '/kbso/kbso-' . $locale . '.mo' );
-	load_plugin_textdomain( 'kbso', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+function kbso_load_textdomain() {
+    
+    load_plugin_textdomain( 'kbso', false, KBSO_PATH . '/languages' );
+    
+}
+add_filter( 'wp_loaded', 'kbso_load_textdomain' );
+
+/*
+ * Check for the required version of PHP
+ */
+if ( version_compare( PHP_VERSION, '5.2', '<' ) ) {
+    
+    if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+        
+        require_once ABSPATH . '/wp-admin/includes/plugin.php';
+        deactivate_plugins(__FILE__);
+        wp_die( __( 'Kebo Social requires PHP 5.2 or higher, as does WordPress 3.2 and higher.', 'kbso' ) );
+        
+    } else {
+        
+        return;
+        
+    }
+    
 }
 
-/**
- * Activate the plugin
+/*
+ * Load Relevant Internal Files
  */
-function kbso_activate() {
-	// First load the init scripts in case any rewrite functionality is being loaded
-	kbso_init();
+function kbso_plugin_setup() {
 
-	flush_rewrite_rules();
+    /*
+     * Include Loader file.
+     */
+    require_once( KBSO_PATH . 'inc/loader.php' );
+    
 }
-register_activation_hook( __FILE__, 'kbso_activate' );
+add_action( 'plugins_loaded', 'kbso_plugin_setup', 15 );
 
 /**
- * Deactivate the plugin
- * Uninstall routines should be in uninstall.php
+ * Register plugin scripts and styles.
  */
-function kbso_deactivate() {
+function kbso_register_files() {
 
+    // Register Styles
+    wp_register_style( 'kbso-admin-css', KBSO_URL . 'assets/css/admin.css', array(), KBSO_VERSION, 'all' );
+    wp_register_style( 'kbso-widgets-css', KBSO_URL . 'assets/css/widgets.css', array(), KBSO_VERSION, 'all' );
+    wp_register_style( 'kbso-sharelinks-css', KBSO_URL . 'assets/css/sharelinks.css', array(), KBSO_VERSION, 'all' );
+        
+    // Register Scripts
+    wp_register_script( 'kbso-admin-js', KBSO_URL . 'assets/js/admin.js', array(), KBSO_VERSION, true );
+    wp_register_script( 'responsive-slides', KBSO_URL . 'assets/js/vendor/responsiveslides.min.js', array( 'jquery' ), KBSO_VERSION, false );
+        
 }
-register_deactivation_hook( __FILE__, 'kbso_deactivate' );
+add_action( 'wp_enqueue_scripts', 'kbso_register_files' );
+add_action( 'admin_enqueue_scripts', 'kbso_register_files' );
+    
+/**
+ * Enqueue frontend plugin scripts and styles.
+ */
+function kbso_enqueue_frontend() {
 
-// Wireup actions
-add_action( 'init', 'kbso_init' );
+    
+        
+}
+add_action( 'wp_enqueue_scripts', 'kbso_enqueue_frontend' );
+    
+/**
+ * Enqueue backend plugin scripts and styles.
+ */
+function kbso_enqueue_backend( $hook_suffix ) {
+        
+    // Enqueue on all pages
+    wp_enqueue_style( 'kbso-admin-css' );
+        
+    // Enqueue files for sharing page
+    if ( 'kebo-social_page_kbso-sharing' == $hook_suffix ) {
+            
+        wp_enqueue_script( 'jquery-ui-sortable' );
+            
+    }
+        
+}
+add_action( 'admin_enqueue_scripts', 'kbso_enqueue_backend' );
 
-// Wireup filters
-
-// Wireup shortcodes
+/**
+ * Add a link to the plugin screen, to allow users to jump straight to the settings page.
+ */
+function kbso_plugin_settings_link( $links ) {
+    
+    $links[] = '<a href="' . admin_url( 'options-general.php?page=kebo-twitter' ) . '">' . __( 'Settings', 'kbso' ) . '</a>';
+    
+    return $links;
+    
+}
+add_filter( 'plugin_action_links_kebo-social/kebo-social.php', 'kbso_plugin_settings_link' ); 
