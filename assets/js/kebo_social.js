10e0d6d5d96d7de239332cab64c9deba2fafa09c
peterbooker <peter@kebopowered.com>
2013-12-27 17:49:20 +0000

    added default asset files from previous kebo social version

---
+++ assets/js/kebo_social.js
@@ -0,0 +1,8 @@
+/*! Kebo Social - v0.1.0
+ * http://kebopowered.com/plugins/kebo-social/
+ * Copyright (c) 2013; * Licensed GPLv2+ */
+( function( window, undefined ) {
+	'use strict';
+
+
+} )( this );
