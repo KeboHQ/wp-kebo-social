c884ea4d37ddedd0da9e5cdcfbe55385a5c224e6
peterbooker <peter@kebopowered.com>
2013-12-29 09:09:27 +0000

    basic version of social sharing admin page

--- inc/modules/social-sharing/menu.php
+++ inc/modules/social-sharing/menu.php
@@ -36,11 +36,84 @@ function kbso_sharing_page_render() {
     
     ?>
 
-    <div class="wrap">
+    <div class="wrap kebo">
         
-        <h2><?php _e( 'Kebo Social - Sharing', 'kbso' ); ?></h2>
+        <h2><?php esc_html_e( 'Kebo Social - Sharing', 'kbso' ); ?></h2>
         <?php settings_errors('kbso-sharing'); ?>
+        
+        <h3><?php esc_html_e('Share Links','kbso'); ?></h3>
+        <p><?php esc_html_e('Add sharing buttons to your website and allow visitors to share your content on social networks.', 'kbso'); ?></p>
+        
+        <div class="share-links-container">
 
+            <div class="available-services share-panel">
+
+                <div class="description">
+                    
+                    <h3><?php esc_html_e('Available Services','kbso'); ?></h3>
+                    
+                    <p><?php esc_html_e('Drag the services you would like to display on your site into the box below.', 'kbso'); ?></p>
+                    
+                </div>
+                
+                <div class="links available">
+                    
+                    <ul id="share-links-available" class="link-container connectedSortable">
+                        
+                        <?php kbso_social_share_services( 'remaining' ); ?>
+                        
+                    </ul>
+                    
+                </div>
+                
+                <div class="clearfix"></div>
+
+            </div>
+
+            <div class="selected-services share-panel">
+                
+                <div class="description">
+                    
+                    <h3><?php esc_html_e('Selected Services','kbso'); ?></h3>
+                    
+                    <p><?php esc_html_e('Services listed here will appear in the same order on your website.', 'kbso'); ?></p>
+                    
+                </div>
+                
+                <div class="links selected">
+                    
+                    <ul id="share-links-selected" class="link-container connectedSortable">
+                        
+                        <?php kbso_social_share_services( 'selected' ); ?>
+                        
+                    </ul>
+                    
+                </div>
+                
+                <div class="clearfix"></div>
+                
+            </div>
+            
+            <div class="preview-services share-panel">
+                
+                <div class="description">
+                    
+                    <h3><?php esc_html_e('Preview','kbso'); ?></h3>
+                    
+                    <p><?php esc_html_e('This is how the links will look on your website.', 'kbso'); ?></p>
+                    
+                </div>
+                
+                <div class="links selected">
+                    
+                </div>
+                
+                <div class="clearfix"></div>
+                
+            </div>
+        
+        </div><!-- .share-links-container -->
+        
         <form method="post" action="options.php">
             <?php
             settings_fields( 'kbso_options' );
@@ -52,5 +125,10 @@ function kbso_sharing_page_render() {
     </div>
 
     <?php
+    
+    /*
+     * Print Sharing Page JS
+     */
+    kbso_sharing_page_print_js();
     
 }
