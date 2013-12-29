eacb07f57e7b2f9e24c064ba8139604273b67460
peterbooker <peter@kebopowered.com>
2013-12-29 08:40:37 +0000

    added uninstall file

---
+++ uninstall.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Uninstall - Removed Options and Transients.
+ */
+
+// Check for Un-Install constant.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
+}
+
+if ( is_multisite() ) {
+
+    global $wpdb;
+
+    // Store Network Site ID so we can get back later.
+    $current_blog = get_current_blog_id();
+
+    // Get a list of all Blog IDs, ignore network admin with ID of 1.
+    $blogs = $wpdb->get_results("
+            SELECT blog_id
+            FROM {$wpdb->blogs}
+            WHERE site_id = '{$wpdb->siteid}'
+            AND spam = '0'
+            AND deleted = '0'
+            AND archived = '0'
+            AND blog_id != '{$current_blog}'
+        ");
+
+    foreach ( $blogs as $blog ) {
+
+        switch_to_blog( $blog->blog_id );
+
+        // Delete the Options we registered.
+        delete_option( 'kbso_plugin_options' );
+        
+    }
+
+    // Go back to Network Site
+    switch_to_blog( $current_blog );
+    
+} else {
+
+    // Delete the Options we registered.
+    delete_option( 'kbso_plugin_options' );
+    
+}
