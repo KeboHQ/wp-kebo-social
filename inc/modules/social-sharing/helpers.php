812d2250b934cc84529061083e879044bb2361fe
peterbooker <peter@kebopowered.com>
2013-12-29 09:06:18 +0000

    added helper function to output social services

--- inc/modules/social-sharing/helpers.php
+++ inc/modules/social-sharing/helpers.php
@@ -1,4 +1,25 @@
 <?php
-/* 
+/*
  * Helper Functions
  */
+
+/*
+ * Output Selected Share Services
+ */
+function kbso_social_share_services( $type = 'selected' ) {
+
+    $services = kbso_share_links_order( $type );
+
+    if ( is_array( $services ) ) {
+
+        foreach ( $services as $link ) {
+            ?>
+            <li class="<?php echo $link; ?> share-link sortable" data-service="<?php echo $link; ?>">
+                <a><i class="zocial <?php echo $link; ?>"></i><span class="name"><?php echo ucfirst($link); ?></span></a>
+            </li>
+            <?php
+        }
+        
+    }
+    
+}
