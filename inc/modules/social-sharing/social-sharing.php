90dc1ef0a0ccaf460ddec9ed7ac73e3a3a1f7cec
peterbooker <peter@kebopowered.com>
2014-01-03 08:09:45 +0000

    added preview function and file enqueuing

--- inc/modules/social-sharing/social-sharing.php
+++ inc/modules/social-sharing/social-sharing.php
@@ -3,6 +3,140 @@
  * Handle choosing which links to show and where.
  */
 
+/**
+ * Register plugin scripts and styles.
+ */
+function kbso_social_sharing_register_files() {
+
+    // Register Styles
+    wp_register_style( 'kbso-sharelinks-min', KBSO_URL . 'inc/modules/social-sharing/assets/css/sharelinks.min.css', array(), KBSO_VERSION, 'all' );
+        
+}
+add_action( 'wp_enqueue_scripts', 'kbso_register_files' );
+add_action( 'admin_enqueue_scripts', 'kbso_register_files' );
+
+/**
+ * Enqueue backend plugin scripts and styles.
+ */
+function kbso_social_sharing_enqueue_backend( $hook_suffix ) {
+    
+    // Enqueue files for sharing page
+    if ( 'kebo-social_page_kbso-sharing' == $hook_suffix ) {
+        
+        wp_enqueue_style( 'kbso-sharelinks' );
+            
+    }
+        
+}
+add_action( 'admin_enqueue_scripts', 'kbso_enqueue_backend' );
+
+/*
+ * Social Sharing Admin Button Preview
+ */
+function kbso_share_button_preview() {
+    
+    $all_links = array(
+        'facebook' => array(
+            'name' => 'facebook',
+            'label' => 'Facebook',
+            'href' => '#'
+        ),
+        'twitter' => array(
+            'name' => 'twitter',
+            'label' => 'Twitter',
+            'href' => '#'
+        ),
+        'googleplus' => array(
+            'name' => 'googleplus',
+            'label' => 'Google+',
+            'href' => '#'
+        ),
+        'linkedin' => array(
+            'name' => 'linkedin',
+            'label' => 'LinkedIn',
+            'href' => '#'
+        ),
+        'pinterest' => array(
+            'label' => 'Pinterest',
+            'name' => 'pinterest',
+            'href' => '#'
+        ),
+    );
+    
+    $selected_links = array();
+    
+    /*
+     * Group the selected Social Links
+     */
+    foreach ( kbso_share_links_order('selected') as $link ) {
+            
+        if ( isset( $all_links[ $link ] ) ) {
+                
+            $selected_links[ $link ] = $all_links[ $link ];
+                
+        }
+            
+    }
+    
+    $options = kbso_get_plugin_options();
+
+    $theme = $options['social_sharing_theme'];
+    
+    /*
+     * All basic themes use the same view files.
+     */
+    $basic_themes = array( 'default', 'flat', 'gradient' );
+    
+    if ( in_array( $theme, $basic_themes ) ) {
+        
+        $theme_view = 'default';
+        
+    } else {
+        
+        $theme_view = $theme;
+        
+    }
+    
+    /**
+     * Setup an instance of the View class.
+     * Allow customization using a filter.
+     */
+    $view = new Kebo_View(
+        apply_filters(
+            'kbso_social_sharing_view_dir',
+            KBSO_PATH . 'inc/modules/social-sharing/views/' . $theme_view
+        )
+    );
+    
+    /**
+     * Prepare the HTML classes
+     */
+    $classes[] = 'ksharelinks';
+    $classes[] = $options['social_sharing_theme'];
+    $classes[] = $options['social_sharing_link_size'];
+    if ( is_rtl() ) {
+        $classes[] = 'rtl';
+    }
+    
+    /*
+     * Build HTML output
+     */
+    $preview_links = $view
+        ->set_view( 'links' )
+        ->set( 'classes', $classes )
+        ->set( 'label', $options['social_sharing_label'] )
+        ->set( 'link_content', $options['social_sharing_link_content'] )
+        ->set( 'options', $options )
+        ->set( 'links', $selected_links )
+        ->set( 'view', $view )
+        ->retrieve();
+    
+    unset( $view );
+    
+    return $preview_links;
+    
+}
+
 /*
  * Social Sharing Admin Page JS
  */
