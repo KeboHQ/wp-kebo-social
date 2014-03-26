71c43bcca73ec3daa4f3762c66faf4fbabda1592
peterbooker <peter@kebopowered.com>
2014-03-26 19:20:41 +0000

    added trial dashboard widget

--- kebo-social.php
+++ kebo-social.php
@@ -94,8 +94,22 @@ function kbso_register_files() {
         
     // Register Scripts
     wp_register_script( 'kbso-admin-js', KBSO_URL . 'assets/js/admin.js', array(), KBSO_VERSION, true );
-    //wp_register_script( 'responsive-slides', KBSO_URL . 'assets/js/vendor/responsiveslides.min.js', array( 'jquery' ), KBSO_VERSION, false );
     wp_register_script( 'jquery-ui-touchpunch', KBSO_URL . 'assets/js/vendor/jquery.ui.touch-punch.min.js', array( 'jquery-ui-sortable' ), KBSO_VERSION, false );
+    
+    wp_register_script( 'flot', KBSO_URL . 'assets/js/vendor/flot/jquery.flot.js', array( 'jquery' ), KBSO_VERSION, false );
+    wp_register_script( 'flot-min', KBSO_URL . 'assets/js/vendor/flot/jquery.flot.min.js', array( 'jquery' ), KBSO_VERSION, false );
+    
+    wp_register_script( 'flot-resize', KBSO_URL . 'assets/js/vendor/flot/jquery.flot.resize.js', array( 'flot' ), KBSO_VERSION, false );
+    wp_register_script( 'flot-resize-min', KBSO_URL . 'assets/js/vendor/flot/jquery.flot.resize.min.js', array( 'flot-min' ), KBSO_VERSION, false );
+    
+    wp_register_script( 'flot-pie', KBSO_URL . 'assets/js/vendor/flot/jquery.flot.pie.js', array( 'flot' ), KBSO_VERSION, false );
+    wp_register_script( 'flot-pie-min', KBSO_URL . 'assets/js/vendor/flot/jquery.flot.pie.min.js', array( 'flot-min' ), KBSO_VERSION, false );
+    
+    wp_register_script( 'flot-canvas', KBSO_URL . 'assets/js/vendor/flot/jquery.flot.canvas.js', array( 'flot' ), KBSO_VERSION, false );
+    wp_register_script( 'flot-canvas-min', KBSO_URL . 'assets/js/vendor/flot/jquery.flot.canvas.min.js', array( 'flot-min' ), KBSO_VERSION, false );
+    
+    wp_register_script( 'flot-time', KBSO_URL . 'assets/js/vendor/flot/jquery.flot.time.js', array( 'flot' ), KBSO_VERSION, false );
+    wp_register_script( 'flot-time-min', KBSO_URL . 'assets/js/vendor/flot/jquery.flot.time.min.js', array( 'flot-min' ), KBSO_VERSION, false );
         
 }
 add_action( 'wp_enqueue_scripts', 'kbso_register_files' );
@@ -115,7 +129,14 @@ add_action( 'wp_enqueue_scripts', 'kbso_enqueue_frontend' );
  * Enqueue backend plugin scripts and styles.
  */
 function kbso_enqueue_backend( $hook_suffix ) {
-        
+    
+    // Enqueue files for core dashboard page
+    if ( 'index.php' == $hook_suffix ) {
+            
+        wp_enqueue_style( 'kbso-admin' );
+            
+    }
+    
     // Enqueue files for dashboard page
     if ( 'toplevel_page_kbso-dashboard' == $hook_suffix ) {
             
@@ -205,3 +226,128 @@ function kbso_admin_menu_styles_print() {
     
 }
 add_action( 'in_admin_footer', 'kbso_admin_menu_styles_print' );
+
+/**
+ * Add a widget to the dashboard.
+ *
+ * This function is hooked into the 'wp_dashboard_setup' action below.
+ */
+function kbso_add_dashboard_widgets() {
+
+	wp_add_dashboard_widget(
+            'kbso_social_sharing_status',
+            __( 'Social Sharing Status', 'kbso' ),
+            'kbso_status_dashboard_widget_render'
+        );	
+}
+add_action( 'wp_dashboard_setup', 'kbso_add_dashboard_widgets' );
+
+/**
+ * Create the function to output the contents of our Dashboard Widget.
+ */
+function kbso_status_dashboard_widget_render() {
+    
+    /*
+     * Store Query Results in Transient to save processing
+     * 
+     * expiry 5 mins
+     * 
+     * TODO: Make this refresh in the background so it never impacts 
+     */
+    if ( false === ( $counts = get_transient( 'kbso_social_sharing_status_widget' . get_current_blog_id() ) ) ) {
+        
+        global $wpdb;
+
+        /*
+         * Query for all share count post_meta
+         * 
+         * TODO: Test on blogs with extreme numbers of posts.
+         */
+        $results = $wpdb->get_results( 
+            "SELECT * FROM $wpdb->postmeta WHERE meta_key = '_kbso_share_counts'"
+        );
+
+        $counts = new stdClass();
+        
+        /*
+         * Loop each post_meta and update total counts
+         */
+        foreach ( $results as $meta ) {
+
+            $post_counts = unserialize( $meta->meta_value );
+
+            if ( isset( $post_counts['total'] ) && is_int( $post_counts['total'] ) ) {
+
+                $counts->total = $counts->total + $post_counts['total'];
+
+            }
+
+            if ( isset( $post_counts['twitter'] ) && is_int( $post_counts['twitter'] ) ) {
+
+                $counts->twitter = $counts->twitter + $post_counts['twitter'];
+
+            }
+
+            if ( isset( $post_counts['facebook'] ) && is_int( $post_counts['facebook'] ) ) {
+
+                $counts->facebook = $counts->facebook + $post_counts['facebook'];
+
+            }
+            
+            if ( isset( $post_counts['linkedin'] ) && is_int( $post_counts['linkedin'] ) ) {
+
+                $counts->linkedin = $counts->linkedin + $post_counts['linkedin'];
+
+            }
+            
+            if ( isset( $post_counts['googleplus'] ) && is_int( $post_counts['googleplus'] ) ) {
+
+                $counts->googleplus = $counts->googleplus + $post_counts['googleplus'];
+
+            }
+
+        }
+        
+        /*
+         * Add custom expiry time for use in soft expiry
+         */
+        $counts->expiry = time() + ( 5 * MINUTE_IN_SECONDS );
+        
+        set_transient( 'kbso_social_sharing_status_widget' . get_current_blog_id(), $counts, 5 * MINUTE_IN_SECONDS );
+        
+    }
+    
+    ?>
+    <div class="kbso-status">
+        
+        <div class="kfull" style="border: none;" title="Total Number of Social Shares">
+            <span class="kcount total" style="font-size: 18px; margin: 0;"><?php echo esc_html( $counts->total ); ?></span>
+            <span class="klabel" style="margin: 0;">Total Shares</span>
+        </div>
+        
+        <div class="khalf">
+            <span class="kcount twitter"><?php echo esc_html( $counts->twitter ); ?></span>
+            <span class="klabel">Twitter</span>
+        </div>
+        
+        <div class="khalf kright">
+            <span class="kcount facebook"><?php echo esc_html( $counts->facebook ); ?></span>
+            <span class="klabel">Facebook</span>
+        </div>
+        
+        <div class="khalf">
+            <span class="kcount googleplus"><?php echo esc_html( $counts->googleplus ); ?></span>
+            <span class="klabel">Google+</span>
+        </div>
+        
+        <div class="khalf kright">
+            <span class="kcount linkedin"><?php echo esc_html( $counts->linkedin ); ?></span>
+            <span class="klabel">LinkedIn</span>
+        </div>
+        
+        <br class="clear">
+        
+    </div>
+    <?php
+        
+} 
