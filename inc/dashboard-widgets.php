8beec0fd95940725a907eb8024e84a17374980ff
peterbooker <peter@kebopowered.com>
2014-03-27 15:34:14 +0000

    improved usability of html output code

--- inc/dashboard-widgets.php
+++ inc/dashboard-widgets.php
@@ -19,20 +19,62 @@ function kbso_dashboard_widget_welcome() {
     
     $title = __( 'Welcome', 'kbso' );
     
-    $content = sprintf( __( 'Hi %s, welcome to your Kebo Social dashboard.', 'kbso' ), $current_user->display_name );
+    // Begin Output Buffering
+    ob_start();
+    ?>
+
+    <p>
+        <?php
+        echo sprintf( __( 'Hi %s, welcome to your Kebo Social dashboard.', 'kbso' ), $current_user->display_name );
+        ?>
+    </p>
     
-    $content .= '<br><br>';
-    
-    $content .= sprintf( __( 'Kebo Social was inspired by the success of the <a href="%s" target="%s">Kebo Twitter Feed</a> plugin.', 'kbso' ), 'http://wordpress.org/plugins/kebo-twitter-feed/', '_blank' );
-    
-    $content .= '&nbsp;';
-    
-    $content .= __( 'We are now dedicated to providing the most hassle-free way to integrate your website with Social Services.', 'kbso' );
+    <p>
+        <?php
+        echo sprintf( __( 'Kebo Social was inspired by the success of the <a href="%s" target="%s">Kebo Twitter Feed</a> plugin.', 'kbso' ), 'http://wordpress.org/plugins/kebo-twitter-feed/', '_blank' );
+        echo '&nbsp;';
+        echo __( 'We are now dedicated to providing the most hassle-free way to integrate your website with Social Services.', 'kbso' );
+        ?>
+    </p>
+
+    <?php
+    // End Output Buffering and Clear Buffer
+    $content = ob_get_contents();
+    ob_end_clean();
     
     kbso_dashboard_widget_render( $title, $content, $sortable = false );
     
 }
 add_action( 'kbso_dashboard_column_one', 'kbso_dashboard_widget_welcome' );
+
+/**
+ * Output 'Feature Control' Widget
+ */
+function kbso_dashboard_widget_feature_control() {
+    
+    $title = __( 'Feature Control', 'kbso' );
+    
+    // Begin Output Buffering
+    ob_start();
+    ?>
+    
+    <form method="post" action="options.php" class="feature-control">
+        <?php
+        settings_fields( 'kbso_options' );
+        do_settings_sections( 'kbso-settings' );
+        submit_button();
+        ?>
+    </form>
+
+    <?php
+    // End Output Buffering and Clear Buffer
+    $content = ob_get_contents();
+    ob_end_clean();
+    
+    kbso_dashboard_widget_render( $title, $content, $sortable = false );
+    
+}
+add_action( 'kbso_dashboard_column_one', 'kbso_dashboard_widget_feature_control' );
 
 /**
  * Output 'Getting Started' Widget
@@ -41,15 +83,28 @@ function kbso_dashboard_widget_getting_started() {
     
     $title = __( 'Getting Started', 'kbso' );
     
-    $content = __( 'Kebo Social is just beginning its life as a WordPress plugin and will be under active development constantly.' , 'kbso' );
+    // Begin Output Buffering
+    ob_start();
+    ?>
     
-    $content .= '&nbsp;';
+    <p>
+        <?php
+        echo __( 'Kebo Social is just beginning its life as a WordPress plugin and will be under active development constantly.' , 'kbso' );
+        echo '&nbsp;';
+        echo sprintf( __( 'You can add Social Share Links to your posts from <a href="%s">here</a>.', 'kbso' ), admin_url( 'admin.php?page=kbso-sharing' ) );
+        ?>
+    </p>
     
-    $content .= sprintf( __( 'You can add Social Share Links to your posts from <a href="%s">here</a>.', 'kbso' ), admin_url( 'admin.php?page=kbso-sharing' ) );
+    <p>
+        <?php
+        echo __( 'These are specially designed to look beautiful, be responsive and include no tracking scripts.', 'kbso' );
+        ?>
+    </p>
     
-    $content .= '<br><br>';
-    
-    $content .= __( 'These are specially designed to look beautiful, be responsive and include no tracking scripts.', 'kbso' );
+    <?php
+    // End Output Buffering and Clear Buffer
+    $content = ob_get_contents();
+    ob_end_clean();
     
     kbso_dashboard_widget_render( $title, $content, $sortable = false );
     
@@ -63,59 +118,56 @@ function kbso_dashboard_widget_about_kebo() {
     
     $title = __( 'About Kebo', 'kbso' );
     
-    $content = __( 'We create hassle-free WordPress plugins which allow you to get more from your websites.' , 'kbso' );
+    // Begin Output Buffering
+    ob_start();
+    ?>
     
-    $content .= '&nbsp;';
+    <p>
+        <?php
+        echo __( 'We create hassle-free WordPress plugins which allow you to get more from your websites.' , 'kbso' );
+        echo '&nbsp;';
+        echo __( 'We thrive on making advanced features accessible and user-friendly.' , 'kbso' );
+        ?>
+    </p>
     
-    $content .= __( 'We thrive on making advanced features accessible and user-friendly.' , 'kbso' );
-    
-    $content .= '<br><br>';
-    
-    $content .= '<div class="more-from" style="width: 50%; float: left;">';
-    
-        $content .= '<h4>' . __( 'More from Kebo', 'kbso' ) . '</h4>';
-
-        $content .= '<ul>';
-
-            $content .= '<li>';
-                $content .= sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://kebopowered.com/plugins/', '_blank', 'Plugins' );
-            $content .= '</li>';
-
-            $content .= '<li>';
-                $content .= sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://kebopowered.com/blog/', '_blank', 'Blog' );
-            $content .= '</li>';
-
-            $content .= '<li>';
-                $content .= sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://kebopowered.com/documentation/', '_blank', 'Support Docs' );
-            $content .= '</li>';
-
-        $content .= '</ul>';
-    
-    $content .= '</div>';
-    
-    $content .= '<div class="follow-us" style="width: 50%; float: right;">';
-    
-        $content .= '<h4>' . __( 'Follow Us', 'kbso' ) . '</h4>';
-
-        $content .= '<ul>';
-
-            $content .= '<li>';
-                $content .= sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://twitter.com/kebopowered/', '_blank', 'Twitter' );
-            $content .= '</li>';
+    <div class="more-from" style="width: 50%; float: left;">
+        
+        <h4><?php _e( 'More from Kebo', 'kbso' ); ?></h4>
+        
+        <ul>
             
-            $content .= '<li>';
-                $content .= sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://www.facebook.com/kebopowered/', '_blank', 'Facebook' );
-            $content .= '</li>';
-
-            $content .= '<li>';
-                $content .= sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://github.com/kebopowered/', '_blank', 'Github' );
-            $content .= '</li>';
+            <li><?php echo sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://kebopowered.com/plugins/', '_blank', 'Plugins' ); ?></li>
             
-        $content .= '</ul>';
+            <li><?php echo sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://kebopowered.com/blog/', '_blank', 'Blog' ); ?></li>
+            
+            <li><?php echo sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://kebopowered.com/documentation/', '_blank', 'Support Docs' ); ?></li>
+            
+        </ul>
+        
+    </div>
     
-    $content .= '</div>';
+    <div class="follow-us" style="width: 50%; float: right;">
+        
+        <h4><?php _e( 'Follow Us', 'kbso' ); ?></h4>
+        
+        <ul>
+            
+            <li><?php echo sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://twitter.com/kebopowered/', '_blank', 'Twitter' ); ?></li>
+            
+            <li><?php echo sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://www.facebook.com/kebopowered/', '_blank', 'Facebook' ); ?></li>
+            
+            <li><?php echo sprintf( __( '<a href="%s" target="%s">%s</a>', 'kbso' ), 'https://github.com/kebopowered/', '_blank', 'Github' ); ?></li>
+            
+        </ul>
+        
+    </div>
     
-    $content .= '<div class="clearfix"></div>';
+    <br class="clear">
+    
+    <?php
+    // End Output Buffering and Clear Buffer
+    $content = ob_get_contents();
+    ob_end_clean();
     
     kbso_dashboard_widget_render( $title, $content, $sortable = false );
     
@@ -129,32 +181,42 @@ function kbso_dashboard_widget_coming_soon() {
     
     $title = __( 'Coming Soon', 'kbso' );
     
-    $content = __( 'In the coming months we will be adding many more features. You can see what to expect next below:' , 'kbso' );
+    // Begin Output Buffering
+    ob_start();
+    ?>
     
-    $content .= '<br>';
+    <p>
+        <?php
+        echo __( 'In the coming months we will be adding many more features. You can see what to expect next below:' , 'kbso' );
+        ?>
+    </p>
     
-    $content .= '<div class="planned-features">';
-
-        $content .= '<ul>';
-
-            $content .= '<li>';
-                $content .= '<h4><strong>' . __( 'Social Feeds', 'kbso' ) . '</strong></h4>';
-                $content .= '<p style="margin-top: 0;">' . __( 'Display your Social Feeds on your website in minutes.', 'kbso' ) . '</p>';
-            $content .= '</li>';
-
-            $content .= '<li>';
-                $content .= '<h4><strong>' . __( 'Social Posting', 'kbso' ) . '</strong></h4>';
-                $content .= '<p style="margin-top: 0;">' . __( 'Share your new content across Social Services instantly.', 'kbso' ) . '</p>';
-            $content .= '</li>';
-
-            $content .= '<li>';
-                $content .= '<h4><strong>' . __( 'Social Widgets', 'kbso' ) . '</strong></h4>';
-                $content .= '<p style="margin-top: 0;">' . __( 'Display your Social Feeds on your website in minutes.', 'kbso' ) . '</p>';
-            $content .= '</li>';
-
-        $content .= '</ul>';
+    <div class="planned-features">
+        
+        <ul>
+            
+            <li>
+                <h4 style="font-weight: bold;"><?php _e( 'Social Feeds', 'kbso' ); ?></h4>
+                <p style="margin-top: 0;">
+                    <?php echo __( 'Display your Social Feeds on your website in minutes.', 'kbso' ); ?>
+                </p>
+            </li>
+            
+            <li>
+                <h4 style="font-weight: bold;"><?php _e( 'Social Posting', 'kbso' ); ?></h4>
+                <p style="margin-top: 0;">
+                    <?php echo __( 'Share your new content across Social Services instantly.', 'kbso' ) ?>
+                </p>
+            </li>
+            
+        </ul>
+        
+    </div>
     
-    $content .= '</div>';
+    <?php
+    // End Output Buffering and Clear Buffer
+    $content = ob_get_contents();
+    ob_end_clean();
     
     kbso_dashboard_widget_render( $title, $content, $sortable = false );
     
@@ -168,37 +230,42 @@ function kbso_dashboard_widget_support() {
     
     $title = __( 'Support', 'kbso' );
     
-    $content = sprintf( __( 'We are actively supporting the plugin and the easiest way to reach us is on the plugins <a href="%s" target="%s">support forum</a>.', 'kbso' ), 'http://wordpress.org/support/plugin/kebo-social/', '_blank' );
+    // Begin Output Buffering
+    ob_start();
+    ?>
     
-    $content .= '&nbsp;';
+    <p>
+        <?php
+        echo sprintf( __( 'We are actively supporting the plugin and the easiest way to reach us is on the plugins <a href="%s" target="%s">support forum</a>.', 'kbso' ), 'http://wordpress.org/support/plugin/kebo-social/', '_blank' );
+        echo '&nbsp;';
+        echo __( 'You may run into bugs but if you let us know we will deal with them straight away.', 'kbso' );
+        ?>
+    </p>
     
-    $content .= __( 'You may run into bugs but if you let us know we will deal with them straight away.', 'kbso' );
+    <p>
+        <?php
+        echo sprintf( __( 'You can also find Support Documentation <a href="%s" target="%s">here</a>, and we have listed some of the most common below: ', 'kbso' ), 'https://kebopowered.com/docs/kebo-social/', '_blank' );
+        ?>
+    </p>
     
-    $content .= '<br><br>';
-    
-    $content .= sprintf( __( 'You can also find Support Documentation <a href="%s" target="%s">here</a>, and we have listed some of the most common below: ', 'kbso' ), 'https://kebopowered.com/docs/kebo-social/', '_blank' );
-    
-    $content .= '<br>';
-    
-    $content .= '<div class="support-docs">';
-
-        $content .= '<ul>';
-
-            $content .= '<li>';
-                $content .= sprintf( __( '<a href="%s" target="%s">Getting Started</a>', 'kbso' ), 'https://kebopowered.com/docs/kebo-social/', '_blank' );
-            $content .= '</li>';
+    <div class="support-docs">
+        
+        <ul>
             
-            $content .= '<li>';
-                $content .= sprintf( __( '<a href="%s" target="%s">Social Sharing - Add a Custom Theme</a>', 'kbso' ), 'https://kebopowered.com/docs/kebo-social/', '_blank' );
-            $content .= '</li>';
-
-            $content .= '<li>';
-                $content .= sprintf( __( '<a href="%s" target="%s">Social Sharing - Add Social Services</a>', 'kbso' ), 'https://kebopowered.com/docs/kebo-social/', '_blank' );
-            $content .= '</li>';
-
-        $content .= '</ul>';
+            <li><?php echo sprintf( __( '<a href="%s" target="%s">Getting Started</a>', 'kbso' ), 'https://kebopowered.com/docs/kebo-social/', '_blank' ); ?></li>
+            
+            <li><?php echo sprintf( __( '<a href="%s" target="%s">Social Sharing - Add a Custom Theme</a>', 'kbso' ), 'https://kebopowered.com/docs/kebo-social/', '_blank' ); ?></li>
+            
+            <li><?php echo sprintf( __( '<a href="%s" target="%s">Social Sharing - Add Social Services</a>', 'kbso' ), 'https://kebopowered.com/docs/kebo-social/', '_blank' ); ?></li>
+            
+        </ul>
+        
+    </div>
     
-    $content .= '</div>';
+    <?php
+    // End Output Buffering and Clear Buffer
+    $content = ob_get_contents();
+    ob_end_clean();
     
     kbso_dashboard_widget_render( $title, $content, $sortable = false );
     
@@ -212,19 +279,34 @@ function kbso_dashboard_widget_get_involved() {
     
     $title = __( 'Get Involved', 'kbso' );
     
-    $content = sprintf( __( 'You can see the plugin source on <a href="%s" target="%s">Github</a>. We welcome involvement from the community and would love to see how we can improve the plugin.', 'kbso' ), 'https://github.com/kebopowered/kebo-social/', '_blank' );
+    // Begin Output Buffering
+    ob_start();
+    ?>
     
-    $content .= '<br><br>';
+    <p>
+        <?php
+        echo sprintf( __( 'You can see the plugin source on <a href="%s" target="%s">Github</a>. We welcome involvement from the community and would love to see how we can improve the plugin.', 'kbso' ), 'https://github.com/kebopowered/kebo-social/', '_blank' );
+        ?>
+    </p>
     
-    $content .= sprintf( __( 'We also welcome anyone willing to translate the plugin, and those interested can find the .pot file <a href="%s" target="%s">here</a>.', 'kbso' ), 'https://github.com/kebopowered/wp-kebo-social/blob/master/languages/kbso.pot', '_blank' );
+    <p>
+        <?php
+        echo sprintf( __( 'We also welcome anyone willing to translate the plugin, and those interested can find the .pot file <a href="%s" target="%s">here</a>.', 'kbso' ), 'https://github.com/kebopowered/wp-kebo-social/blob/master/languages/kbso.pot', '_blank' );
+        echo '&nbsp;';
+        echo __( 'Before you get started, please be aware that the plugin will be growing quickly in the coming months so translations will need to be updated.' , 'kbso' );
+        ?>
+    </p>
     
-    $content .= '&nbsp;';
+    <p>
+        <?php
+        echo sprintf( __( 'If you find any bugs please let me know using either the <a href="%s" target="%s">Support Forum</a> or Github <a href="%s" target="%s">Issues</a>.', 'kbso' ), 'http://wordpress.org/support/plugin/kebo-social', '_blank', 'https://github.com/kebopowered/wp-kebo-social/issues', '_blank' );
+        ?>
+    </p>
     
-    $content .= __( 'Before you get started, please be aware that the plugin will be growing quickly in the coming months so translations will need to be updated.' , 'kbso' );
-    
-    $content .= '<br><br>';
-    
-    $content .= sprintf( __( 'If you find any bugs please let me know using either the <a href="%s" target="%s">Support Forum</a> or Github <a href="%s" target="%s">Issues</a>.', 'kbso' ), 'http://wordpress.org/support/plugin/kebo-social', '_blank', 'https://github.com/kebopowered/wp-kebo-social/issues', '_blank' );
+    <?php
+    // End Output Buffering and Clear Buffer
+    $content = ob_get_contents();
+    ob_end_clean();
     
     kbso_dashboard_widget_render( $title, $content, $sortable = false );
     
