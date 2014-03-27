61233912d8c8b8b47c66867665a5be114bf06acc
peterbooker <peter@kebopowered.com>
2014-03-27 19:37:26 +0000

    added assets

---
+++ assets/js/feature-control.js
@@ -0,0 +1,58 @@
+/** 
+ * Controls the activation and de-activation of Features in real-time.
+ * 
+ * Uses Ajax to update options.
+ */
+
+jQuery(document).ready(function($) {
+
+    $(".kebo div.switch").click(function(e) {
+
+        var kslug = $(this).data('id');
+
+        var data = {
+            action: 'kbso_feature_control_update',
+            data: kslug,
+            nonce: keboFeatures.nonce
+        };
+
+        if ($(this).children('input#x1').is(':checked')) {
+
+            // Update the Option with AJAX
+            $.post(ajaxurl, data, function(response) {
+
+                response = $.parseJSON(response);
+
+                if ('true' === response.success && 'save' === response.action) {
+
+                    $('h2.nav-tab-wrapper').append('<a class="nav-tab" data-slug="' + kslug + '" href="' + keboFeatures.url + kslug + '" style="display: none;">Sharing</a>');
+                    $('h2.nav-tab-wrapper a[data-slug=' + kslug + ']').fadeIn(400);
+
+                }
+
+            });
+
+
+
+        } else {
+
+            // Update the Option with AJAX
+            $.post(ajaxurl, data, function(response) {
+
+                response = $.parseJSON(response);
+
+                if ('true' === response.success && 'save' === response.action) {
+
+                    $('h2.nav-tab-wrapper a[data-slug=' + kslug + ']').fadeOut(400, function() {
+                        $(this).remove();
+                    });
+
+                }
+
+            });
+
+        }
+
+    });
+
+});
