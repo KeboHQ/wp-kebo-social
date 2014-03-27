a04d659607e7a781439b0b1149e47cd2a04b064b
peterbooker <peter@kebopowered.com>
2014-03-27 22:32:41 +0000

    updated assets

---
+++ assets/js/social-sharing.js
@@ -0,0 +1,13 @@
+/** 
+ * Social Sharing - Controls the selection of social services and live updating of the preview.
+ * 
+ * Uses Ajax to save selected services and position.
+ */
+
+jQuery(document).ready(function($) {
+
+    /*
+     * TODO: Move the JS here and use wp_localize_script() to pass data.
+     */
+
+});
